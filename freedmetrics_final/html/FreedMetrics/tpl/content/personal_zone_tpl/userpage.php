2ab849d94bbcb41217e3f0dfc0b22d503a0c375b
Miguel Luengo Perez <44966857+mluengo@users.noreply.github.com>
2020-03-16 16:54:52 +0100

    Add files via upload

--- freedmetrics_final/html/FreedMetrics/tpl/content/personal_zone_tpl/userpage.php
+++ freedmetrics_final/html/FreedMetrics/tpl/content/personal_zone_tpl/userpage.php
@@ -1,19 +1,42 @@
 <?php
 
-$header = "UserPage";
-$subheader = "Wellcome";
+include_once("history.php");
+
+$header = "User Page";
+$subheader = "Check your last queries";
 $title = "User Name";
 
 print(page_head($_SESSION['pagename'],$title));
-
 print(page_header($header, $subheader,1));
+
 ?>
-<!-- Main Content -->
+
 <div class="container">
-	<div class="row">
-		<div class="col-lg-8 col-md-10 mx-auto">
-			<h2>Error 404: Page Not Found</h2>
+	<div class="col-lg- <?php print $lg_width;?> col-md-<?php print $lg_width;?> mx-auto">
+	
+		<div class="row">
+			<?php
+
+				$entries = user_history($_SESSION['person_id']);
+				$tag = "History";
+				
+				if (!count($entries)) {
+				   	print ("<h4 style=\"text-align: center;\">No articles found.</h4>
+				   			<p style=\"text-align: center;\"><i>It seems that you have not searched any article yet.</i><p>");
+				} else {
+			?>
+			
+			<h5>User History: <?php print count($entries);?> queries found</h5>
+	    	<?php print results_table($entries,$tag); }?>
 		</div>
+		
+		<div class="row"><p><br><br></p></div>
+
+		<div class="row">
+	    	<div class="clearfix">
+    			<a class="btn btn-primary float-left" href="index.php?pagename=Advanced-Search&new=1">Search more articles</a>
+			</div>
+		</div>
+
 	</div>
 </div>
-<?php print(page_footer($_SESSION['pagename']));
