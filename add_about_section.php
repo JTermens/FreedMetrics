c5588289964defa929bc592a1f631812077f6d85
oscar-camacho <57529702+oscar-camacho@users.noreply.github.com>
2020-03-17 21:18:31 +0100

    Add files via upload

---
+++ add_about_section.php
@@ -0,0 +1,23 @@
+<section class="page-section bg-dark" id="about">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-8 text-center">
+        <h2 class="text-white mt-0">About us</h2>
+        <hr class="divider light my-4">
+        <p class="text-white-50 mb-4">FreedMetrics is a web service created by a multidisciplinary group of 4 students from the University Pompeu Fabra (UPF) of Barcelona coursing the MSc in Bioinformatics for Health Sciences.
+        <p class="text-white-50 mb-4">We believe that the impact of scientific articles is a valuable information for any student and researcher. Our philosophy is that access to information should be free and easy for everyone.
+          <div class="clearfix">
+               <a class="btn btn-primary" href="link_to_authors_page">Meet our team</a>
+          </div>
+          <br>
+          <br>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-lg-8 text-center">
+        <h2 class="text-white mt-0">Institutions</h2>
+            <img src="<link_to_upf_logo>" alt="logo" width="40%">
+      </div>
+    </div>
+  </div>
+</section>
