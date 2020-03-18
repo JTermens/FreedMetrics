d87b186a77187ed29a163eea9f560e9bdc918a9f
JTermens <55915367+JTermens@users.noreply.github.com>
2020-03-18 18:30:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once('inc/globals.inc.php');
+
+if(array_key_exists('pagename', $_GET)){
+    $pagename = $_GET['pagename'];
+    $_SESSION['pagename'] = $pagename;
+
+    if (array_key_exists($pagename, $pagename_ref)) {
+        include $pagename_ref[$pagename]['route'];
+    }else {
+        include $pagename_ref['Not-Found']['route'];
+    }
+
+}else{
+    header('Location: index.php?pagename=Home');
+}
