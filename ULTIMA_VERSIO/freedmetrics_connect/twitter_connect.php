c4cd08682b83d0fefaebe6b5a19076ad24f96cda
JTermens <55915367+JTermens@users.noreply.github.com>
2020-03-18 18:31:08 +0100

    Add files via upload

---
+++ ULTIMA_VERSIO/freedmetrics_connect/twitter_connect.php
@@ -0,0 +1,10 @@
+<?php
+#Global variables for the different functions:
+/** Tokens from the Twitter app for verification **/
+$settings = array(
+    'oauth_access_token' => "1223942603042885643-Pig1Q1zY6XILhMBic7ZWClQcO8cvYF",
+    'oauth_access_token_secret' => "HcXwn6O2EXhKc7bsWbbo9j430h3l6MhAVEjVT1Bc41YOj",
+    'consumer_key' => "53A869T3xK58tossGG07SA8xr",
+    'consumer_secret' => "T2EOFr3woryLDauJNEjCpxSnC1eyi1clxzMRl3KX41XwSDiW1j",
+    'tweet_mode' => "extended"
+);
